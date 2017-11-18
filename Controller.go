e119772bfaec3b1ac0b38999a956b5733c4212e4
ketan <ketank90@gmail.com>
2017-11-18 23:17:14 +0530

    More routers in Cotroller

--- Controller.go
+++ Controller.go
@@ -4,16 +4,44 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"encoding/json"
+	"log"
 )
 
 func main() {
-	router:=mux.NewRouter()
-	router.HandleFunc("/rooms",GetRoom).Methods("GET")
-	router.HandleFunc("/rooms",PutRoom).Methods("PUT")
-	router.HandleFunc("/rooms",DeleteRoom).Methods("DELETE")
+	router:=Router()
+	Rooms(*router)
+	Users(*router)
 
-	http.ListenAndServe(":1234",router);
+	router.HandleFunc("/rooms", GetRoom).Methods("GET")
+log.Print("Rooms API")
+	http.ListenAndServe(":1234", router);
+
+
 }
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	return router
+
+}
+func Rooms(router mux.Router) mux.Router{
+	router.HandleFunc("/rooms", GetRoom).Methods("GET")
+	router.HandleFunc("/rooms", PutRoom).Methods("PUT")
+	router.HandleFunc("/rooms", DeleteRoom).Methods("DELETE")
+
+	return router
+
+}
+func Users(router mux.Router) {
+	router.HandleFunc("/users", GetUser).Methods("GET")
+
+
+}
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Users")
+
+}
+
 func DeleteRoom(writer http.ResponseWriter, r *http.Request) {
 
 }
@@ -23,5 +51,5 @@ func PutRoom(writer http.ResponseWriter, r *http.Request) {
 
 func GetRoom(w http.ResponseWriter, r *http.Request) {
 
-	json.NewEncoder(w).Encode("people")
+	json.NewEncoder(w).Encode("Rooms")
 }
